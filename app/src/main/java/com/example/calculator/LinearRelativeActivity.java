3b369b4d12f192d484e24d8879f6416e6b618130
Sukhovolskij.Andrew <asuhovolskij65@gmail.com>
2019-04-01 03:00:37 +0300

    Code Style + 3 listeners

--- app/src/main/java/com/example/calculator/LinearRelativeActivity.java
+++ app/src/main/java/com/example/calculator/LinearRelativeActivity.java
@@ -1,7 +1,13 @@
 package com.example.calculator;
 
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import butterknife.OnClick;
 
 public class LinearRelativeActivity extends AppCompatActivity {
 
@@ -9,5 +15,19 @@ public class LinearRelativeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_linear_relative);
+        }
+        @OnClick({R.id.btn_main_seven,R.id.btn_main_eight,R.id.btn_main_nine})
+        public void onButtonClick(Button button) {
+            switch(button.getId()){
+                case R.id.btn_main_seven:
+                    break;
+                case R.id.btn_main_eight:
+                    break;
+                case R.id.btn_main_nine:
+                    break;
+            }
+        }
+
+
     }
-}
+
